9764088f0e1e49648021cafc1f4befe809604b15
Christopher De Santiago <chris@ChristophersMBP.lan>
2023-04-15 16:13:03 -0700

    completed... I think

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const locationButton = document.getElementById("formSubmit");
 
 locationButton.addEventListener('click', e =>{
     const weatherField = document.querySelector('.weatherInformation');
+    //const locationName = document.getElementById("locationInput").value;
     e.preventDefault();
-    locationButton.value = '';
     weatherField.innerHTML = '';
     getWeather();
     console.log("Button Clicked");
@@ -19,18 +19,23 @@ async function getWeather(){
 
 function displayWeatherInfo(data){
     const weatherField = document.querySelector('.weatherInformation');
+    //const weatherHeader = document.querySelector('#weatherHeader');
     const header = document.createElement('h2');
     const tempF = document.createElement('p');
     const humidity = document.createElement('p');
     const winds = document.createElement('p');
     const skys = document.createElement('p');
+    const skyImage = document.createElement('img');
+    skyImage.src = data.current.condition.icon;
+
     header.innerText = data.location.name+", "+data.location.region;
     skys.innerText = data.current.condition.text;
-    tempF.innerText = "Current: "+data.current.temp_f+ " \u00B0F" +" / "+data.current.temp_c+ " \u00B0C";
+    tempF.innerText = "Temperature: "+data.current.temp_f+ " \u00B0F" +" / "+data.current.temp_c+ " \u00B0C";
     humidity.innerText = "Humidity: " + data.current.humidity+"%";
     winds.innerText = "Wind: " + data.current.wind_mph+" mph" + " / " + data.current.wind_kph+" kph";
 
     weatherField.appendChild(header);
+    weatherField.appendChild(skyImage);
     weatherField.appendChild(skys);
     weatherField.appendChild(tempF);
     weatherField.appendChild(humidity);
@@ -39,6 +44,28 @@ function displayWeatherInfo(data){
 
 function fieldEmpty(div){
     if(div.innerHTML === ""){
-        
+
     }
 }
+
+function showTime(){
+
+    const timeDiv = document.querySelector(".currentTime");
+
+    const date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    var dateString = new Date().toLocaleString("en-US", {timeZone:'America/Los_Angeles'});
+    var time = dateString.replace(", ", " - ");
+
+    let currentDate = `${month}-${day}-${year}`;
+
+    timeDiv.innerText = time;
+}
+
+setInterval(showTime,1000);
+
+showTime();
